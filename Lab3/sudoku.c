c4aa566994d1a4a7948ae1a5f99fb17ed935a238
andrewselvarajah <andrew.selvarajah@uoit.net>
2018-03-07 22:41:50 +0000

    working loaded sudoku

--- Lab3/sudoku.c
+++ Lab3/sudoku.c
@@ -8,50 +8,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <stdbool.h>
 #include <dirent.h>
 #include <sys/types.h>
 #include <string.h>
 
+FILE *file;
+int c;
+bool check = false;
+int row ;
+int column;
+int board[9][9];
+
+
+void loadFile(){
+    file = fopen("puzzle.txt", "r");
+    if (file != NULL) {
+		for (int row = 0; row < 9; row++) { // loop for each row
+			for (int col = 0; col < 9; col++) {
+				fscanf(file, "%d ", &board[row][col]);
+			}
+		}
+	} else {
+		printf("Could not open file\n");
+	}
+	fclose(file);
+
+}
+void checkColumn(){
+
+}
+void checkRow(){
+
+}
+void checkSubGrid(){
+
+}
+void checkSudoku(){
+
+}
 int main()
 {
-    FILE *file;
-    int c;
-    bool check = false;
-    int counter = 0;
-    int counter2 = 0;
-    file = fopen("puzzle.txt", "r");
-    int board[9][9];
+   row = 0;
+   column = 0;
+   
+    loadFile();
 
-    if (file)
-    {
-        while (((c = getc(file)) != EOF))
-        {
-            if (check == false)
-            {
-
-                if (counter == 9)
-                {
-                    counter2++;
-                    counter = 0;
-                }
-                board[counter][counter2];
-                printf("%d\n", board[counter][counter2]);
-                counter++;
-                check = true;
-            }
-            else if (check == true)
-            {
-                check = false;
-            }
-        }
-    }
-    /*for (int x = 0; x < 9; x++)
+   
+    for (int x = 0; x < 9; x++)
     {
         for (int y = 0; y < 9; y++)
         {
             printf("%d", board[x][y]);
+            printf(" ");
         }
         printf("\n");
-    }*/
+    }
 }
+
