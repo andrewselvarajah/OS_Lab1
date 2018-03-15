52bf44e70de6016da8b9b2d689a2826c1e8f1adb
andrewselvarajah <andrew.selvarajah@uoit.net>
2018-03-15 18:36:55 +0000

    added lab4 base

--- Lab3/sudoku.c
+++ Lab3/sudoku.c
@@ -17,9 +17,11 @@
 FILE *file;
 int c;
 bool check = false;
-int row ;
-int column;
+int rowChecks[9];
+int columnChecks[9];
+int gridChecks[9];
 int board[9][9];
+
 
 
 void loadFile(){
@@ -37,25 +39,97 @@ void loadFile(){
 
 }
 void checkColumn(){
+    for(int col = 0; col < 9; col++){
+        int counter[9] = {0};
+		for(int row = 0; row < 9; row++){
+			if(board[row][col] != 0){
+				counter[board[row][col]-1] += 1;
+			}
+		}
+		columnChecks[col] = 1;
+		for(int i = 0; i < 9; i++){
+			if(counter[i] > 1){
+				columnChecks[col] = 0;
+				break;
+			}
+		}
+	}
+	return NULL;
 
 }
 void checkRow(){
+    for(int row = 0; row < 9; row++){
+		int counter[9] = {0};
+		for(int col = 0; col < 9; col++){
+			if(board[row][col] != 0){
+				counter[board[row][col]-1] += 1;
+			}
+		}
+		rowChecks[row] = 1;
+		for(int i = 0; i < 9; i++){
+			if(counter[i] > 1){
+				rowChecks[row] = 0;
+				break;
+			}
+		}
+	}
+	return NULL;
 
 }
-void checkSubGrid(){
+int checkSubGrid(int rowNum, int column){
 
+    int counter[9] = {0};
+	for(int row = rowNum; row < rowNum+3; row++){
+		for(int col = column; col < column+3; col++){
+			if(board[row][col] != 0){
+				counter[board[row][col]-1] += 1;
+			}
+		}
+	}
+	for(int i = 0; i < 9; i++){
+		if(counter[i] > 1){
+			return 0;
+		}
+	}
+	return 1;
+
+}
+void* validateGrids () {
+	for (int row = 0; row < 9; row+=3) {
+		for (int col = 0; col < 9; col+=3) {
+			gridChecks[row+col/3] = checkSubGrid(row, col);
+		}
+	}
+	return NULL;
 }
 void checkSudoku(){
+    pthread_t columns, rows, grids;
 
+	// create threads and execute thread functions
+	pthread_create(&columns, NULL, checkColumn, NULL);
+	pthread_create(&rows, NULL, checkRow, NULL);
+	pthread_create(&grids, NULL, checkSubGrid, NULL);
+
+	pthread_join(columns, NULL);
+	pthread_join(rows, NULL);
+	pthread_join(grids, NULL);
+
+	for (int i = 0; i < 9; i++) {
+		if (columnChecks[i] == 0)
+			return 0;
+		if (rowChecks[i] == 0)
+			return 0;
+		if (gridChecks[i] == 0)
+			return 0;
+	}
+	return 1;
 }
+
 int main()
 {
-   row = 0;
-   column = 0;
    
     loadFile();
 
-   
     for (int x = 0; x < 9; x++)
     {
         for (int y = 0; y < 9; y++)
